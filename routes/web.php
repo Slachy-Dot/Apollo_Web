8b965b349b2280a6c6c70d5b70f6af220741c59a
Slachy <Slachy@apollo.com>
2023-04-13 05:26:14 +0200

    add dynmap router

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::get('/blog', [PageController::class, 'Blog'])->name('index');
 Route::get('/home', function () {
     return view('Pages.Home.index');
 
+Route::get('/home', function () {
+    return view('Pages.Dynmap.index');    
 });
